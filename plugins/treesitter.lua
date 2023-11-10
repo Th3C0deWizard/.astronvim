f123cfc3d296894db0d1cf59e486860d66ae69cb
Carlos Paez <carlosa.paezc@autonoma.edu.co>
2023-11-10 11:29:18 -0500

    change

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -5,8 +5,5 @@ return {
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       -- "lua"
     })
-    opts.autotag = {
-      enable = true,
-    }
   end,
 }

aec9238a95f86fc2408880d27266a055a41aa683
Carlos Paez <carlosa.paezc@autonoma.edu.co>
2023-11-10 11:18:02 -0500

    autoTagClose and neo-tree settings added

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -5,5 +5,8 @@ return {
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       -- "lua"
     })
+    opts.autotag = {
+      enable = true,
+    }
   end,
 }
